2b42f72d16a07b957a304df8aedf04b75825efb2
Thiago <122575310+othiagojacare@users.noreply.github.com>
2024-02-13 21:30:26 -0300

    primeira versao calculadora de imc

---
+++ script.js
@@ -0,0 +1,23 @@
+function calcular(){
+    var altura = document.getElementById('altura').value
+    var peso = document.getElementById('peso').value
+
+    var res = Number(peso / (altura * altura))
+
+    if(res <= 18.5){
+        alert('Abaixo do peso')
+    }else if ( res <=  24.9){
+        alert('Peso normal')
+    }else if(res >= 29.9){
+        alert('Sobrepeso')
+    }else if(res >= 34.9){
+        alert('Obesidade grau 1')
+    }else if(res >= 39.9){
+        alert('Obesidade grau 2')
+    }else if(res >= 40){
+        alert('Obesidade grau 3')
+    }
+
+    alert(res.toFixed(2))
+
+}
